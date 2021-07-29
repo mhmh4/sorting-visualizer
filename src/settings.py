e1655001f28db9256700bcf1b771256d7945521d
u06 <u06_m@protonmail.com>
2021-07-29 19:13:06 -0400

    set to recommended settings

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,8 @@
-BLOCK_SIZE = 25
+BLOCK_SIZE = 10
 
 # Screen dimensions
 WIDTH = 1000
-HEIGHT = 300
+HEIGHT = 500
 
 # Every rectangle will have at least a 1 block gap around its perimeter
 WIDTH += BLOCK_SIZE
@@ -12,4 +12,4 @@ NUM_RECTANGLES = ((WIDTH - BLOCK_SIZE) // BLOCK_SIZE) // 2
 MAX_RECT_HEIGHT_POSSIBLE = (HEIGHT - BLOCK_SIZE) // BLOCK_SIZE
 
 # For larger screen dimensions, decrease the delay time
-DELAY_MS = 50 # in milliseconds
+DELAY_MS = 13 # in milliseconds
