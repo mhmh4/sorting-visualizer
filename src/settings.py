2c3cfd31f63d48c6936e5d2e2ea91c15e7125bc7
u06 <86537561+u06@users.noreply.github.com>
2021-08-06 18:22:10 -0400

    increased delay to 20 ms

--- src/settings.py
+++ src/settings.py
@@ -12,4 +12,4 @@ NUM_RECTANGLES = ((WIDTH - BLOCK_SIZE) // BLOCK_SIZE) // 2
 MAX_RECT_HEIGHT_POSSIBLE = (HEIGHT - BLOCK_SIZE) // BLOCK_SIZE
 
 # For larger screen dimensions, decrease the delay time
-DELAY_MS = 13 # in milliseconds
+DELAY_MS = 20
