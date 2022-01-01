c4cb6378208382b47669306cfaac766ec2e3bf32
miah <86537561+u06@users.noreply.github.com>
2022-01-01 07:48:28 -0500

    Fixed comments

--- src/settings.py
+++ src/settings.py
@@ -1,15 +1,14 @@
 BLOCK_SIZE = 10
 
-# Screen dimensions
+# Game window dimensions
 WIDTH = 1000
 HEIGHT = 500
 
-# Every rectangle will have at least a 1 block gap around its perimeter
+# To account for putting a 1 block gap around every rectangle's perimeter
 WIDTH += BLOCK_SIZE
 HEIGHT += BLOCK_SIZE
 
 NUM_RECTANGLES = ((WIDTH - BLOCK_SIZE) // BLOCK_SIZE) // 2
 MAX_RECT_HEIGHT_POSSIBLE = (HEIGHT - BLOCK_SIZE) // BLOCK_SIZE
 
-# For larger screen dimensions, decrease the delay time
-DELAY_MS = 20
+DELAY = 20
