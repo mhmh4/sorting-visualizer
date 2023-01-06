c432ca9f6e648c9c2b05f82fd8c175895ee4463f
miah <86537561+mhmh4@users.noreply.github.com>
2023-01-05 22:05:21 -0500

    Let algorithm selection be done on the command line

--- src/main.py
+++ src/main.py
@@ -2,22 +2,40 @@ import sys
 
 import pygame
 
-from generate import generate_random_array
-from menu import get_selection
+import generate
+import algorithms
 
 
 def main():
-    array = generate_random_array()
+    if len(sys.argv) < 2:
+        print("Usage: sorting algorithm name or id")
+        return
+    choice = sys.argv[1].lower()
+    match choice:
+        case "1" | "bubble":    selection = algorithms.bubble_sort
+        case "2" | "counting":  selection = algorithms.counting_sort
+        case "3" | "heap":      selection = algorithms.heapsort
+        case "4" | "insertion": selection = algorithms.insertion_sort
+        case "5" | "merge":     selection = algorithms.merge_sort
+        case "6" | "quick":     selection = algorithms.quicksort
+        case "7" | "selection": selection = algorithms.selection_sort
+        case _:
+            print(f"Invalid selection: '{choice}'")
+            return
+
+    array = generate.generate_random_array()
     done = False
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        if not done:
-            selected_algorithm_function = get_selection()
-            selected_algorithm_function(array)
-        done = True
+        try:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            if not done:
+                selection(array)
+            done = True
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == "__main__":
