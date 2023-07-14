35ba28746bf73e69084ae9e4d45789b9050d2b68
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-14 12:19:20 -0400

    Only use algorithm name when choosing an algorithm

--- main.py
+++ main.py
@@ -10,22 +10,29 @@ import algorithms
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: sorting algorithm name or id")
+        print("Usage: sorting algorithm name")
         return
     choice = sys.argv[1].lower()
     match choice:
-        case "1" | "bubble":    selection = algorithms.bubble_sort
-        case "2" | "counting":  selection = algorithms.counting_sort
-        case "3" | "heap":      selection = algorithms.heapsort
-        case "4" | "insertion": selection = algorithms.insertion_sort
-        case "5" | "merge":     selection = algorithms.merge_sort
-        case "6" | "odd-even":  selection = algorithms.odd_even_sort
-        case "7" | "quick":     selection = algorithms.quicksort
-        case "8" | "selection": selection = algorithms.selection_sort
+        case "bubble":
+            selection = algorithms.bubble_sort
+        case "counting":
+            selection = algorithms.counting_sort
+        case "heap":
+            selection = algorithms.heapsort
+        case "insertion":
+            selection = algorithms.insertion_sort
+        case "merge":
+            selection = algorithms.merge_sort
+        case "odd-even":
+            selection = algorithms.odd_even_sort
+        case "quick":
+            selection = algorithms.quicksort
+        case "selection":
+            selection = algorithms.selection_sort
         case _:
             print(f"Invalid selection: '{choice}'")
             return
-
     array = generate.generate_random_array()
     done = False
     while True:
